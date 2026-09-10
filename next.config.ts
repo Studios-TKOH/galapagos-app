522136cac30e9c6de75d9d6f364462a13447883c
mikyy12 <70312623+mikyy12@users.noreply.github.com>
2026-09-10 17:35:02 -0500

    chore(next): hide development indicator

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
+  devIndicators: false,
+  images: {
     remotePatterns: [
       {
         protocol: "https",
@@ -9,7 +10,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  allowedDevOrigins: ['192.168.100.27', '192.168.18.5'],
+  allowedDevOrigins: ["192.168.100.27", "192.168.18.5"],
 };
 
 export default nextConfig;
